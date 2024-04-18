fb1448f565cee8be64b07e51d9edc368775c748c
Vu Nguyen <vunguyen4920@gmail.com>
2024-04-18 09:47:12 +0700

    Initial setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,23 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "eslint",
+  "volar",
+  "tsserver",
+  "lemminx",
+  "yamlls",
+  "jsonls",
+  "prismals",
+  "graphql",
+  "marksman",
+  "dockerls",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +28,40 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.volar.setup {}
+
 -- typescript
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+    "javascript.jsx",
+    "typescript.tsx",
+    "vue",
+  },
+}
+
+lspconfig.eslint.setup {
+  on_init = on_init,
+  capabilities = capabilities,
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
 }
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,90 @@
+-- These are some examples, uncomment them if you want to see them work!
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "lua-language-server",
+        "stylua",
+
+        -- web-dev
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "vue-language-server",
+        "lemminx",
+        "yaml-language-server",
+        "json-lsp",
+
+        -- db
+        "prisma-language-server",
+        "graphql-language-service-cli",
+
+        -- documentation
+        "marksman",
+
+        -- general purpose
+        "typescript-language-server",
+
+        -- devops
+        "dockerfile-language-server",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+
+        -- web-dev
+        "html",
+        "css",
+        "tsx",
+        "vue",
+        "xml",
+        "yaml",
+        "json",
+        "json5",
+        "jsonc",
+
+        -- db
+        "prisma",
+        "graphql",
+
+        -- documentation
+        "markdown",
+        "mermaid",
+        "jsdoc",
+        "luadoc",
+
+        -- general purpose
+        "regex",
+        "javascript",
+        "typescript",
+
+        -- devops
+        "dockerfile",
+
+        -- git
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+      },
+    },
+  },
+}
+
+return plugins
