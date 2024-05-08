33fce50a0d40c315c5a4d897cc12eab87af568bc
Vu Nguyen <vunguyen4920@gmail.com>
2024-05-08 09:19:00 +0700

    Update lspconfig installation

--- README.md
+++ README.md
@@ -4,11 +4,53 @@
 
 - Neovim 0.9.5.
 - Nerd Font as your terminal font.
-- Make sure the nerd font you set doesn't end with Mono to prevent small icons. 
-    - Example : JetbrainsMono Nerd Font and not JetbrainsMono Nerd Font Mono
+- Make sure the nerd font you set doesn't end with Mono to prevent small icons.
+  - Example : JetbrainsMono Nerd Font and not JetbrainsMono Nerd Font Mono
 - Ripgrep is required for grep searching with Telescope (OPTIONAL).
 - GCC, Windows users must have mingw installed and set on path.
 - Make, Windows users must have GnuWin32 installed and set on path.
 - Delete old neovim folders (check commands below)
+
+## Installation
+
+### Linux
+
+```bash
+
+git clone https://github.com/vunguyen4920/my-NvChad ~/.config/nvim && nvim
+
+```
+
+### Windows
+
+```cmd
+
+git clone https://github.com/vunguyen4920/my-NvChad %USERPROFILE%\AppData\Local\nvim && nvim
+
+```
+
+```powershell
+
+git clone https://github.com/vunguyen4920/my-NvChad $ENV:USERPROFILE\AppData\Local\nvim && nvim
+
+```
+
+## Uninstallation
+
+```shell
+
+# Linux / Macos (unix)
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+
+# Windows CMD
+rd -r ~\AppData\Local\nvim
+rd -r ~\AppData\Local\nvim-data
+
+# Window PowerShell
+rm -Force ~\AppData\Local\nvim
+rm -Force ~\AppData\Local\nvim-data
+
+```
 
 ## Plugins
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,49 @@
 -- These are some examples, uncomment them if you want to see them work!
 local plugins = {
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
+    "williamboman/mason.nvim",
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
     lazy = true,
     event = "VeryLazy",
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
-    end,
     opts = {
-      -- defaults
-      "lua_ls",
+      ensure_installed = {
+        -- defaults
+        "lua_ls",
 
-      -- web-dev
-      "html",
-      "cssls",
-      "eslint",
-      "volar",
-      "lemminx",
-      "yamlls",
-      "jsonls",
+        -- web-dev
+        "html",
+        "cssls",
+        "eslint",
+        "volar",
+        "lemminx",
+        "yamlls",
+        "jsonls",
 
-      -- db
-      "prismals",
-      "graphql",
+        -- db
+        "prismals",
+        "graphql",
 
-      -- documentation
-      "marksman",
+        -- documentation
+        "marksman",
 
-      -- general purpose
-      "tsserver",
+        -- general purpose
+        "tsserver",
 
-      -- devops
-      "dockerls",
+        -- devops
+        "dockerls",
+      },
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
   },
   {
     "mfussenegger/nvim-lint",
@@ -71,26 +73,19 @@ local plugins = {
     end,
   },
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {},
+    "rshkarin/mason-nvim-lint",
     dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-tool-installer").setup {
-        ensure_installed = {
-          -- defaults
-          "stylua",
-          "selene",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "selene",
 
-          -- web-dev
-          "prettier",
-          "eslint_d",
-        },
-      }
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {},
+        -- web-dev
+        "eslint_d",
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -149,6 +144,21 @@ local plugins = {
     config = function()
       require "configs.conform"
     end,
+  },
+  {
+    "zapling/mason-conform.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "stylua",
+
+        -- web-dev
+        "eslint_d",
+      },
+    },
   },
   {
     "windwp/nvim-ts-autotag",
