51e77c85ea62d272a889f1036cc8480972f35e48
Vu Nguyen <vunguyen4920@gmail.com>
2024-05-27 14:02:56 +0700

    feat(inlay-hint): Add keys to toggle

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -30,10 +30,15 @@ local servers = {
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(args)
+    local map = vim.keymap.set
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client and client.server_capabilities.inlayHintProvider then
       vim.lsp.inlay_hint.enable(true)
     end
+
+    map("n", "iht", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+    end)
   end,
 })
 
