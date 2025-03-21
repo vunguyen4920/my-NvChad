40f13a16f8dbc207febba67b72091d1c0d799ef3
vu.nl <vu.nl@gianty.com.vn>
2025-03-21 08:35:52 +0700

    cursors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,11 @@ local plugins = {
   {
     "sphamba/smear-cursor.nvim",
     event = "BufEnter",
-    opts = {},
+    opts = {
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.3      [0, 1]
+      distance_stop_animating = 0.8, -- 0.1      > 0
+    },
   },
 }
 
