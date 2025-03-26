2fe9e9619fb3117fe6cbfdc4c46e57c142f9a0ce
vu.nl <vu.nl@gianty.com.vn>
2025-03-26 14:19:49 +0700

    remove symbols usage

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -484,13 +484,6 @@ local plugins = {
       },
     },
   },
-  {
-    "Wansmer/symbol-usage.nvim",
-    event = "LspAttach", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-    config = function()
-      require("symbol-usage").setup()
-    end,
-  },
 }
 
 return plugins
